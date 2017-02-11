a3069e5887f43dd34bee6dde54dfeee27bd187a2
hello年 <1041693529@qq.com>
2017-02-11 09:01:38 +0800

    新建

---
+++ public/js/index.js
@@ -0,0 +1,47 @@
+var app = angular.module('myapp',['ngRoute']);
+        app.config(['$routeProvider',function ($routeProvider) {
+            $routeProvider
+            .when('/home',{templateUrl:'html/home.html',controller:'homeControl'})
+            .when('/search',{templateUrl:'html/seek.html',controller:'seekControl'})
+            .when('/car',{templateUrl:'html/car.html',controller:'carControl'})
+            .when('/mine',{templateUrl:'html/mine.html'})
+            .otherwise({redirectTo:'html/home'})
+        }])
+        app.controller('tab',function ($scope) {
+            // console.log(shopArr.length);
+            // $scope.count = shopArr;
+            $scope.arr = [
+                {
+                    cla:'tabbar-img1-1',
+                    word:'首页',
+                    index:'0',
+                    href:'#/home'
+                },
+                {
+                    cla:'tabbar-img2',
+                    word:'搜索',
+                    index:'1',
+                    href:'#/search'
+                },
+                
+                {
+                    cla:'tabbar-img3',
+                    word:'购物车',
+                    index:'2',
+                    href:'#/car'
+                },
+                {
+                    cla:'tabbar-img4',
+                    word:'我的',
+                    index:'3',
+                    href:'#/mine'
+                }
+            ];
+
+            $scope.cl = function (index) {
+                    for(var i = 0; i < $scope.arr.length; i++) {
+                        $scope.arr[i].cla = 'tabbar-img'+(i+1)
+                    }
+                    $scope.arr[index].cla = $scope.arr[index].cla + '-1'
+            }
+        })
