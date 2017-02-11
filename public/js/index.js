4e6accc195842d9514a2e63c65c73ee0921eddae
hello年 <1041693529@qq.com>
2017-02-11 16:36:43 +0800

    合并

--- public/js/index.js
+++ public/js/index.js
@@ -3,6 +3,7 @@ var app = angular.module('myapp',['ngRoute']);
             $routeProvider
             .when('/home',{templateUrl:'html/home.html',controller:'homeControl'})
             .when('/hm2',{templateUrl:'html/role.html',controller:'hm2Control'})
+            .when('/hm4',{templateUrl:'html/prop.html',controller:'hm4Control'})
             .when('/search',{templateUrl:'html/search.html',controller:'searchControl'})
             .when('/car',{templateUrl:'html/car.html'})
             .when('/mine',{templateUrl:'html/mine.html'})
